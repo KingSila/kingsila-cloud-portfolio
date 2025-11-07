a770caf477df8ad73f63c6e16d83692bf29d8526
Silas Mokone <44573982+KingSila@users.noreply.github.com>
2025-11-07 22:58:03 +0200

    added notes for day1

--- infra/modules/vnet/main.tf
+++ infra/modules/vnet/main.tf
@@ -34,10 +34,3 @@ resource "azurerm_subnet" "subnets" {
   address_prefixes     = each.value.address_prefixes
 }
 
-output "vnet_id" {
-  value = azurerm_virtual_network.vnet.id
-}
-
-output "subnet_ids" {
-  value = { for k, s in azurerm_subnet.subnets : k => s.id }
-}
