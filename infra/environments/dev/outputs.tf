c3fade5bb3911ef677aa2a09610ab182f61be522
Silas Mokone <44573982+KingSila@users.noreply.github.com>
2025-11-16 18:33:43 +0200

    last test for the day

--- infra/environments/dev/outputs.tf
+++ infra/environments/dev/outputs.tf
@@ -29,6 +29,6 @@ output "appinsights_connection_string" {
 
 output "ci_smoke_test" {
   value       = "hello-from-pr"
-  description = "Harmless output to test CI. "
+  description = "Harmless output to test CI. last one"
 }
 
