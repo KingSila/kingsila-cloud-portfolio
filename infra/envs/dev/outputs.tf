db2472a4435832e2c43b5af366b2b7d86b49d543
Silas Mokone <44573982+KingSila@users.noreply.github.com>
2025-12-19 10:08:03 +0200

    fix:added node resource group

--- infra/envs/dev/outputs.tf
+++ infra/envs/dev/outputs.tf
@@ -17,9 +17,9 @@ output "identity_id" {
 # 7. Optional Outputs (handy for debugging)
 #######################################################
 
-output "dev_resource_group_name" {
-  description = "Dev resource group name"
-  value       = azurerm_resource_group.rg.name
+output "node_resource_group_name" {
+  description = "AKS node resource group name"
+  value       = module.aks.node_resource_group
 }
 
 output "aks_cluster_name" {
